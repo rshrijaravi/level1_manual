69610022434ffba9e83784467f64363d0c2770d3
rshrijaravi <rshrija7@gmail.com>
2025-08-20 15:42:40 +0530

    Add files via upload

---
+++ py2.py
@@ -0,0 +1,14 @@
+print("takshashila university")
+print("Ongur,tindivanam,villupuram dist,tn")
+print("---------------------")
+print("student Mark list")
+print("-----------------")
+str1=input("Enter the Register no:")
+str2=input("Enter the name:")
+m1=int(input("python Mark:"))
+m2=int(input("DBMS mark:"))
+m3=int(input("Accounting mark:"))
+total=m1+m2+m3
+print("total mark:",total)
+avg=total/3
+print("Average mark:",avg)
