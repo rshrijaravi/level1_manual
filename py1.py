4b37b4b0e7496f744b2cb64fc3df585c43c56c82
rshrijaravi <rshrija7@gmail.com>
2025-08-20 15:33:05 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,15 @@
+print("ARITHEMATIC OPERATIONS")
+print("---------------------")
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("Result:")
+c=a+b
+print("Add:",c)
+c=a-b
+print("Sub:",c)
+c=a*b
+print("Null:",c)
+c=a/b
+print("Division:",c)
+c=a%b
+print("Modul:",c)
