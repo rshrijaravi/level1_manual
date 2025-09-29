71ca9e83cc180d0fc8dfbc507702cc0a0c7c5d12
rshrijaravi <rshrija7@gmail.com>
2025-09-29 10:35:20 +0530

    Add files via upload

---
+++ py11.py
@@ -0,0 +1,38 @@
+print("Takshashila university")
+print("ongur,tindivanam")
+print("--------------")
+print("student mark list")
+print("-----------------")
+m1=int(input("Enter python mark:"))
+m2=int(input("Enter dbms mark:"))
+m3=int(input("Enter Accounting mark:"))
+total=m1+m2+m3
+print("total mark:",total)
+avg=total/3
+print("Average mark:",avg)
+if
+ m1>=40 and m2>=40 and m3>=40:
+    print("result:pass")
+ if
+ total>=250:
+print("Grade:0 Grade*")
+eleif total>=200:
+eleif total>=180:
+print("Grade:a Grade*")
+else:
+ print("Grade:A Grade*")
+else:
+ print("Grade:B Grade*")
+else:
+    print("Result:Fail")
+    print("Grade:No Grade(Faild)")
+
+
+
+
+
+
+
+
+        
+          
